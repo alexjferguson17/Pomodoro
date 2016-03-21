2c8fb2945d1e133b4b1a0ee06038b27cbb1295b5
Alex Ferguson <alexjferguson17@gmail.com>
2016-03-21 11:24:13 -0400

    Added sound.

--- js/scripts/controllers/MainCtrl.js
+++ js/scripts/controllers/MainCtrl.js
@@ -1,40 +1,42 @@
 (function() {
   function MainCtrl($interval) {
-    TIMER = 1500;
+    TIMER = 10;
     
+    var mySound = new buzz.sound( "/sounds/ding.mp3", {
+      preload: true
+    });
 
 	 	var vm = this;
     vm.count = 0;
 
     vm.timer = TIMER;
-    vm.break = 300;
+    vm.break = 5;
     vm.state = "paused";
 
     var zebra, lion;
 
     this.startTimer = function(){
-      if(vm.count == 4){
-          vm.count = 0
+      if(vm.count === 4){
+          vm.count = 0;
       }
 
-      console.log('Click Works');
-      vm.state = "going"
+      vm.state = "going";
       zebra = $interval(countDown, 1000);
-    }
+    };
 
     this.startBreak = function(){
       lion =  $interval(breakCountdown, 1000);
-    }
+    };
 
     function countDown () {
-      console.log("Interval was called")
       if(vm.timer == 0){
-        vm.state = "breaktime"
+        vm.state = "breaktime";
 
         vm.count += 1;  
 
-        
-        $interval.cancel(zebra)
+        mySound.play();
+
+        $interval.cancel(zebra);
         vm.timer = TIMER;
       }else{
         vm.timer --;
@@ -43,11 +45,12 @@
     }
 
     function breakCountdown () {
-      if(vm.break == 0) {
-        vm.state = "paused"
-        $interval.cancel(lion)
+      if(vm.break === 0) {
+        vm.state = "paused";
+        $interval.cancel(lion);
         vm.break = 2;
-        if(vm.count == 3) {
+        mySound.play();
+        if(vm.count === 3) {
           vm.break = 1800;
         }
       }else {
@@ -59,8 +62,9 @@
       vm.timer = TIMER;
       $interval.cancel(zebra);
       vm.state = "paused";
-   }   
+   };   
   }
+
   angular
       .module('blocPom')
       .controller('MainCtrl', ['$interval', MainCtrl]);
